7b056009624ea3225ba00e335cf4480e5342e444
hchiam <hchiam@gmail.com>
2020-05-18 13:23:21 -0400

    make it talk when ready

--- public/index.js
+++ public/index.js
@@ -1,1 +1,8 @@
-alert("Hi!");
+say("Interface up and running.", console.log);
+
+function say(sentence, sentenceCallback) {
+  if (sentence != "") {
+    responsiveVoice.speak(sentence, "UK English Male");
+    if (sentenceCallback) sentenceCallback(sentence);
+  }
+}
