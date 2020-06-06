e305e36745e05f2ec8bdfc9b593efee2603d65e2
hchiam <hchiam@gmail.com>
2020-06-05 21:30:44 -0400

    clean up (deprecated request -> axios that works nicer with async/await)

--- public/google.js
+++ public/google.js
@@ -1,42 +1,61 @@
-const request = require("request");
+const axios = require("axios");
 const cheerio = require("cheerio");
 
+// Example usage:
+
 // (async function () {
-//   console.log(await google("js sort backwards"));
+//   const query = "js sort backwards";
+//   let queryBiasedToJs = query;
+//   function biasToJs() {
+//     const words = queryBiasedToJs.split(" ");
+//     const hasJs = words.some(
+//       (w) => w.toLowerCase() === "js" || w.toLowerCase() === "javascript"
+//     );
+//     if (hasJs) return;
+//     queryBiasedToJs = "js " + queryBiasedToJs;
+//   }
+//   biasToJs();
+//   console.log(await google(queryBiasedToJs));
 // })();
+
+// TODO: add tests
 
 async function google(what) {
   const url = `https://www.google.com/search?q=${what}`;
-  async function doRequest(url) {
-    return new Promise(function (resolve, reject) {
-      const howManyResults = 3;
-      const searchResults = []; // {header:'', link:'', description:''}
-      request(url, function (err, res, bod) {
-        const body = cheerio.load(bod);
-        body("h3")
-          .slice(0, howManyResults)
-          .each(function () {
-            const header = body(this);
-            const headerText = header.text();
-            const link = header.parent();
-            const linkText = "https://www.google.com/" + link.attr("href");
-            const descriptionSelector = "div.BNeawe div.BNeawe";
-            const description = header
-              .parent()
-              .parent()
-              .siblings()
-              .find(descriptionSelector);
-            const descriptionText = description.text();
-            const searchResult = {
-              header: headerText,
-              link: linkText,
-              description: descriptionText,
-            };
-            searchResults.push(searchResult);
-          });
-        resolve(searchResults);
+  try {
+    const html = await axios.get(url);
+    const howManyResults = 3;
+    const searchResults = []; // {header:'', link:'', description:''}
+    const body = await cheerio.load(html.data);
+    body("h3")
+      .slice(0, howManyResults)
+      .each(function () {
+        const header = body(this);
+        const headerText = header.text();
+        const link = header.parent();
+        const linkText = "https://www.google.com/" + link.attr("href");
+        const descriptionSelector = "div.BNeawe div.BNeawe";
+        const description = header
+          .parent()
+          .parent()
+          .siblings()
+          .find(descriptionSelector);
+        const descriptionText = description.text();
+        const searchResult = {
+          header: headerText,
+          link: linkText,
+          description: descriptionText,
+        };
+        searchResults.push(searchResult);
       });
-    });
+    return searchResults;
+  } catch (error) {
+    return error;
   }
-  return await doRequest(url);
+}
+
+if (typeof exports !== "undefined") {
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = { google };
+  }
 }
