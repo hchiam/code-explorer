fc506f21cfc059ec6e28ae6821584b778401ebc2
hchiam <hchiam@users.noreply.github.com>
2020-06-14 23:00:27 -0400

    find another way to prevent responsive voice from polluting console log
    (also ...message instead of message to use all parameters)

--- public/console.js
+++ public/console.js
@@ -1,13 +1,15 @@
 let logger = console.log;
-console.log = (message) => {
-  if (!responsiveVoiceIsRunning || consoleLogOverride) {
-    logger(message);
+console.log = (...message) => {
+  const responsiveVoiceIsRunning = message[0] && message[0].voiceURI;
+  if (!responsiveVoiceIsRunning) {
+    logger(...message);
   }
   // TODO: can read any regular console.log message here
 };
 let error = console.error;
 console.error = (message) => {
-  if (!responsiveVoiceIsRunning || consoleLogOverride) {
+  const responsiveVoiceIsRunning = message[0] && message[0].voiceURI;
+  if (!responsiveVoiceIsRunning) {
     error(message);
   }
   // TODO: can read any regular console.log message here
