38ba42761b305c66cfbdca7730eb6028776ce7d7
hchiam <hchiam@users.noreply.github.com>
2020-06-23 21:20:07 -0400

    modularize code

--- public/use-ann.js
+++ public/use-ann.js
@@ -19,18 +19,24 @@ const METRIC = "Angular";
 const storage = new Annoy(DIMENSIONS, METRIC);
 
 const v1 = [-5.0, -4.5, -3.2, -2.8, -2.1, -1.5, -0.34, 0, 3.7, 6];
-storage.addItem(0, v1);
-storage.addItem(1, [5.0, 4.5, 3.2, 2.8, 2.1, 1.5, 0.34, 0, -3.7, -6]);
-storage.addItem(2, [0, 0, 0, 0, 0, -1, -1, -0.2, 0.1, 0.8]);
-storage.build(); // no more items can be added after .build()
-storage.save(STATIC_STORAGE_PATH);
-console.log("number of items in storage", storage.getNItems());
 
+init();
 read();
+
+function init() {
+  storage.addItem(0, v1);
+  storage.addItem(1, [5.0, 4.5, 3.2, 2.8, 2.1, 1.5, 0.34, 0, -3.7, -6]);
+  storage.addItem(2, [0, 0, 0, 0, 0, -1, -1, -0.2, 0.1, 0.8]);
+  storage.build(); // no more items can be added after .build()
+  storage.save(STATIC_STORAGE_PATH);
+  // can't .unbuild() after .save()
+  console.log("number of items in storage", storage.getNItems());
+}
 
 function read() {
   if (storage.load(STATIC_STORAGE_PATH)) {
     const kNN = 5;
+    // test getting nearest neighbors to v1:
     const { neighbors, distances } = storage.getNNsByVector(v1, kNN, -1, true); // true = include distances
     // console.log(neighbors); // indices of K nearest neighbors
     console.log(distances);
