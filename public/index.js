8fa41c89789a9a254d3585160971fa0cb34b6861
hchiam <hchiam@users.noreply.github.com>
2020-06-23 22:06:39 -0400

    embedded first API automatically retrieved!

--- public/index.js
+++ public/index.js
@@ -3,21 +3,33 @@ var say;
 var sendShellCommand;
 var getApiTargetObject;
 var getApi1LevelDeep;
+var addSpaces;
 if (typeof require !== "undefined") {
   const { say } = require("./say.js");
   const { sendShellCommand } = require("./shell.js");
   const { getApiTargetObject, getApi1LevelDeep } = require("./api-search.js");
+  const { addSpaces } = require("./sentence-with-spaces.js");
 }
 
 startup();
 
 async function startup() {
   say("Up and running.");
+
   say("Running tests, excluding TFJS.");
   await runStartupTests();
 
-  say("Running an API test.", {
-    sentenceCallback: runApiTest,
+  say("Testing.", {
+    sentenceCallback: async () => {
+      const api = runApiTest();
+      const sentence = addSpaces(api[0].key);
+      console.log("embed this sentence:", sentence);
+      const embedding = await embed1Sentence(sentence);
+      console.log("embedding", embedding);
+      say(
+        `Embedding shown in console log for the following sentence: "${sentence}"`
+      );
+    },
   });
 
   // const sentence = api[0].key; // document.body's api[0].key is "text"
@@ -40,15 +52,24 @@ async function runStartupTests() {
   });
 }
 
+function test() {
+  console.log("api START");
+  const api = runApiTest();
+  console.log("api DONE");
+  // const embedding = embed1Sentence(api[0]);
+  // console.log(embedding);
+}
+
 function runApiTest() {
   const object = getApiTargetObject("button");
   const api = getApi1LevelDeep(object);
   console.log("API: ", api);
   const functionsCount = api.filter((x) => x.type === "function").length;
   const objectsCount = api.filter((x) => x.type === "object").length;
-  say(
+  console.log(
     `API has ${api.length} results, with ${functionsCount} functions and ${objectsCount} objects. Details in console log.`
   );
+  return api;
 }
 
 async function embed1Sentence(sentence, callback) {
