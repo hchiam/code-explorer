feeb60a1cb45e2d960e89f8a74c1f520fd03a45e
hchiam <hchiam@users.noreply.github.com>
2020-07-06 21:52:32 -0400

    slightly less flaky test

--- tests/google.test.js
+++ tests/google.test.js
@@ -16,7 +16,9 @@ test("google results", async function () {
       "https://www.google.com//url?q=https://www.w3schools.com/jsref/jsref_reverse.asp"
     );
   expect(linkStartsCorrectly).toBe(true);
-  expect(results[0].description).toBe(
-    "The reverse() method reverses the order of the elements in an array. Note: this method will change the original array."
-  );
+  expect(
+    results[0].description.includes(
+      "The reverse() method reverses the order of the elements in an array. Note: this method will change the original array."
+    )
+  ).toBe(true);
 });
